1d02652ecaf0eaa9862c244914fd5d262d18a6ed
baotran14 <tranquocbao14@gmail.com>
2024-02-29 23:27:27 +0700

    fix no property

--- vars/regressionTest.groovy
+++ vars/regressionTest.groovy
@@ -1,11 +1,12 @@
-def lib = library('my-shared-lib').tutorial.jenkins
 def call() {
     node {
+        def lib = library('my-shared-lib').tutorial.jenkins
+        def log = lib.Log
         stage("Log Info") {
-            lib.Log.info("This is log for information")
+            log.info("This is log for information")
         }
         stage("Log Warning") {
-            lib.Log.warning("This is log for warning")
+            log.warning("This is log for warning")
         }
     }
 }
