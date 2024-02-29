dc43f8eaea66d597dd2d5a355b4bbdbf24e950bf
baotran14 <tranquocbao14@gmail.com>
2024-02-29 22:54:26 +0700

    test

--- vars/regressionTest.groovy
+++ vars/regressionTest.groovy
@@ -1,12 +1,14 @@
-@Library('my-shared-lib') import tutorial.jenkins.Log
-def log = new Log();
+//@Library('my-shared-lib') import tutorial.jenkins.Log
+//def log = new Log();
 def call() {
     node {
         stage("Log Info") {
-            log.info("This is log for information")
+//            log.info("This is log for information")
+            echo "This is log for information"
         }
         stage("Log Warning") {
-            log.warning("This is log for warning")
+//            log.warning("This is log for warning")
+            echo "This is log for warning"
         }
     }
 }
