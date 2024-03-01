755a726ee623e3a880942f6cef3c353392c95e4a
BaoTran14 <tranquocbao14@gmail.com>
2024-03-01 10:52:00 +0700

    fix error

--- vars/regressionTest.groovy
+++ vars/regressionTest.groovy
@@ -1,13 +1,16 @@
+import tutorial.jenkins.*
+
 def call(arg) {
-//    node {
+    MyLog myLog = new MyLog()
+    node {
         stage("Build:") {
             echo "Building...${arg}"
         }
         stage("Info") {
-            arg.info("This is log for information")
+            myLog.info("This is log for information")
         }
         stage("Warning") {
-            arg.warning("This is log for warning")
+            myLog.warning("This is log for warning")
         }
-//    }
+    }
 }
